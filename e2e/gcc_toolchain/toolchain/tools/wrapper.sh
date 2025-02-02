9db77c823f0e950f43f15292404868bbedd05a98
lummax <lummax@mailbox.org>
2025-02-02 18:06:41 +0100

    feat: add gcc toolchain e2e test

---
+++ e2e/gcc_toolchain/toolchain/tools/wrapper.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+# This wraps gcc and post-processes the generated dotd file to make all
+# included system headers relative paths.
+#
+# Even though we pass -no-canoncical-prefixes/-fno-canonical-system-headers to gcc,
+#  it prints absolute include paths.
+#
+# The `gcc` from Ubuntu/Debian is patched in a way, that `-no-canonical-prefixes`
+# has no effect. Bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=102803
+# patch: https://sources.debian.org/src/gcc-13/13.3.0-12/debian/patches/canonical-cpppath.diff
+
+# This wrapper requires the actual compiler to be passed as the first argument
+readonly gcc="$1"; shift
+
+readonly dotd="$(echo "$@" | grep -o "\-MF [^ ]*.d" | cut -d' ' -f2)"
+"${gcc}" "$@"
+readonly ret=$?
+if [[ -n "${dotd}" ]] then
+  sed -i "s:$(realpath $(pwd))/::" "${dotd}"
+fi
+exit $ret
