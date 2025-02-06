c962be7ad68377c2d7abc3696bdcebc09e967567
lummax <lummax@mailbox.org>
2025-02-06 12:34:26 +0100

    ci: rework/setup release pipeline

---
+++ .github/workflows/release.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+
+# Arguments provided by workflow caller, see ci.yaml
+# step: Build release artifacts and prepare release notes
+readonly TAG="$1"
+readonly PREFIX="$2"
+readonly RELEASE_NOTES="$3"
+
+# NB: configuration for 'git archive' is in /.gitattributes
+git archive --prefix="${PREFIX}-${TAG}/" ${TAG} -o "${PREFIX}-${TAG}.tar.zst"
+
+cat > "${RELEASE_NOTES}" << EOF
+## Using Bzlmod with Bazel 8 or greater
+
+2. Add to your \`MODULE.bazel\` file:
+
+\`\`\`starlark
+bazel_dep(name = "linux_packages", version = "${TAG}")
+\`\`\`
+EOF
+
+tar xf "${PREFIX}-${TAG}.tar.zst"
+
+(
+    pushd e2e/smoke
+    bazel test "--override_module=linux_packages=../../${PREFIX}-${TAG}" //...
+    popd
+)
