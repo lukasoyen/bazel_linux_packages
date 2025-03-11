ef06a8f149ac5d8150d6521ff80c056f4481d641
lummax <lummax@mailbox.org>
2025-03-11 10:48:32 +0100

    refactor: update e2e/toolchains/gcc example

--- e2e/toolchains/gcc/tools/wrapper.sh
+++ e2e/toolchains/gcc/tools/wrapper.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/bash
 
-# This wraps gcc and post-processes the generated dotd file to make all
-# included system headers relative paths.
-#
-# Even though we pass -no-canoncical-prefixes/-fno-canonical-system-headers to gcc,
-#  it prints absolute include paths.
-#
-# The `gcc` from Ubuntu/Debian is patched in a way, that `-no-canonical-prefixes`
-# has no effect. Bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=102803
-# patch: https://sources.debian.org/src/gcc-13/13.3.0-12/debian/patches/canonical-cpppath.diff
+# This wraps gcc and post-processes the generated dotd file to make all included
+# system headers relative paths. The `gcc` from Ubuntu/Debian is patched in a
+# way that `-no-canonical-prefixes` has no effect.
+# https://sources.debian.org/src/gcc-13/13.3.0-12/debian/patches/canonical-cpppath.diff
 
-# This wrapper requires the actual compiler to be passed as the first argument
-readonly gcc="$1"; shift
+set -euo pipefail
 
-readonly dotd="$(echo "$@" | grep -o "\-MF [^ ]*.d" | cut -d' ' -f2 || echo "")"
-"${gcc}" "$@"
-readonly ret=$?
-if [[ -n "${dotd}" ]]; then
-  sed -i "s:$(realpath $(pwd))/::" "${dotd}"
-fi
-exit $ret
+function relativize_dotd() {
+    dotd="$(echo "$@" | grep -o "\-MF [^ ]*.d" | cut -d' ' -f2 || echo "")"
+
+    if [[ -n ${dotd} ]]; then
+        sed -i "s:$(realpath "$(pwd)")/::" "${dotd}"
+    fi
+}
+
+trap 'relativize_dotd "$@"' EXIT
+
+GCC_EXEC_PREFIX="$(dirname "$(which "$(basename "$0")")")/../../usr/lib/gcc/" \
+    "$(basename "$0")" "$@"
+exit $?
